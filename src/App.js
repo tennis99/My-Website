5a8956c94ab0082d3f77df5ff8bdcafdb6c9204f
JOVAN SIHOTA <jovansihota1@gmail.com>
2022-10-31 20:21:22 -0700

    grammar  commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,27 @@
 import React from 'react';
 import Me from './components/Me';
-//import Exp from './components/Exp';
 import Footer from './components/Footer';
 import TitleP from './components/TitleP';
-
 import MySkills from './components/MySkills';
-
-
 import timelineElements from "./timelineElements";
-
-import {
-  VerticalTimeline,
-  VerticalTimelineElement,
-} from "react-vertical-timeline-component";
-
+import {VerticalTimeline, VerticalTimelineElement} from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
-
 
 function App() {
   return (
     <div>
-      
       <TitleP />
       <div >
       <h2 data-aos='fade-up' className="text-3xl py-[3rem] text-center px-4 font-bold text-white">My Work Experience</h2>
       <VerticalTimeline>
         {timelineElements.map((element) => {
 
-          return (
-            
+          return (           
             <VerticalTimelineElement
               key={element.key}
               date={element.date}
               dateClassName="date"    
             >
-              
               <h3 className="vertical-timeline-element-title font-bold text-2xl">
                 {element.title}
               </h3>
@@ -47,10 +34,8 @@ function App() {
         })}
       </VerticalTimeline>
     </div>
-      
       <MySkills />
       <Me />
-      {/* <Exp /> */}
       <Footer />
     </div>
   );
--- src/timelineElements.js
+++ src/timelineElements.js
@@ -4,7 +4,7 @@ let timelineElements = [
       title: "QA Analyst/Developer",
       location: "James Evans & Associates Ltd",
       description:
-        "Worked on automating cross platform testscirpts in Katalon using Groovy. To test the PENFAX-5 software for our three different clients SHEPP, OPTS and PEBA. Worked in a scrum/agile environment which allowed me to work closely with the front-end developers, back-end developers, buisness analyst and computer architects to help point out issues regarding data setup, button functionality and lable functionality.",
+        "Worked on automating cross-platform test scripts in Katalon using Groovy. To test the PENFAX-5 software for our three different clients SHEPP, OPTS and PEBA. Worked in a scrum/agile environment which allowed me to work closely with the front-end developers, back-end developers, business analysts and computer architects to help point out issues regarding data setup, button functionality and label functionality.",
       date: "September 2022 - Present",
       icon: "work",
     },
@@ -13,16 +13,16 @@ let timelineElements = [
       title: "Data Analyst/Developer",
       location: "Vancouver Airport Authority",
       description:
-        "Analyzed historical data and made programs in java to help automate the excel data sheet to my manager's specific needs. While programming in Qlik sense to automate desired data into live charts. That was used for staffing purposes to see where the employees in the airport terminal should put their interests.",
+        "Analyzed historical data and made programs in java to help automate the excel data sheet to my manager's specific needs. While programming in Qlik sense to automate desired data into live charts. That was used for staffing purposes to see where the employees in the airport terminal should be staffed..",
        date: "May 2022 - August 2022",
       icon: "work",
     },
     {
       id: 3,
-      title: "Adminstrative Assistant",
+      title: "Administrative Assistant",
       location: "The InsureBC Group",
       description:
-        "Implemented and started  the 'paperless project' at InsureBC (Deepcove).  This included scanning, sorting and electronically filing all hard copy correspondence regarding clients insurance policies.  Created codes and processess for easy data filing all while ensuring that the codes are identifiable and information is easily accessible for viewing by all insurance advisor's. The other aspect of my role is to upload/update job postings and search for potential prospects through various websites to help fill vacant positions.",
+        "Implemented and started the 'paperless project' at InsureBC (Deepcove).  This included scanning, sorting and electronically filing all hard-copy correspondence regarding clients’ insurance policies.  Created codes and processes for easy data filing all while ensuring that the codes are identifiable and information is easily accessible for viewing by all insurance advisors. The other aspect of my role is to upload/update job postings and search for potential prospects through various websites to help fill vacant positions.",
       date: "Febuary 2019 - Present",
       icon: "work",
     },
